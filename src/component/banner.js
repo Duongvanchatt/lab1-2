2d5969a8d384d1af7be55f41dacc7302070f6345
Duongvanchatt <97280208+Duongvanchatt@users.noreply.github.com>
2022-01-14 21:02:50 +0700

    Update banner.js

--- src/component/banner.js
+++ src/component/banner.js
@@ -2,7 +2,7 @@ const banner = {
     render(){
         return `
         <div class="w-5/6 m-auto mt-2 h-80">
-        <img class="h-full w-full" src="https://lab1-2-2s915n60l-duongvanchatt.vercel.app/" alt="">
+        <img class="h-full w-full" src="https://caodang.fpt.edu.vn/wp-content/uploads/Banner-Web-7.png" alt="">
         </div>
         `;
     },
