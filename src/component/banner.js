0ba180557550c1c4f8057be76ed24ff707a9b405
Duongvanchatt <97280208+Duongvanchatt@users.noreply.github.com>
2022-01-14 21:00:45 +0700

    Update banner.js

--- src/component/banner.js
+++ src/component/banner.js
@@ -2,7 +2,7 @@ const banner = {
     render(){
         return `
         <div class="w-5/6 m-auto mt-2 h-80">
-        <img class="h-full w-full" src="./img/banner.png" alt="">
+        <img class="h-full w-full" src="https://lab1-2-2s915n60l-duongvanchatt.vercel.app/" alt="">
         </div>
         `;
     },
