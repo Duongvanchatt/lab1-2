b1a295ce0866e3e2c0ba6106ac5499b094bae509
Duongvanchatt <97280208+Duongvanchatt@users.noreply.github.com>
2022-01-14 21:10:22 +0700

    Update newList.js

--- src/component/newList.js
+++ src/component/newList.js
@@ -6,12 +6,12 @@ const newList = {
         <h1 class="font-semibold">TIN TỨC HỌC TẬP</h1><br>
         <div class="flex justify-between">
            <div class="w-48 h-auto ">
-            <img class="w-full" src="./img/sp1.jfif" alt="">
+            <img class="w-full" src="http://channel.mediacdn.vn/prupload/879/2018/08/img20180813114743979.jpg" alt="">
             <h2 class="text-lg text-red-500">Sinh viên Fpoly được nghỉ dịch học online.</h2>
             <p class="text-sm mt-2">Sinh viên Fpoly được nghỉ dịch học online tại nhà inh viên Fpoly được nghỉ dịch học online tại nhàinh viên Fpoly được nghỉ dịch học online tại nhà</p>
            </div>
            <div class="w-48 h-auto ">
-            <img class="w-full" src="./img/sp1.jfif" alt="">
+            <img class="w-full" src="http://channel.mediacdn.vn/prupload/879/2018/08/img20180813114743979.jpg" alt="">
             <h2 class="text-lg text-red-500">Sinh viên Fpoly được nghỉ dịch học online.</h2>
             <p class="text-sm mt-2">Sinh viên Fpoly được nghỉ dịch học online tại nhà inh viên Fpoly được nghỉ dịch học online tại nhàinh viên Fpoly được nghỉ dịch học online tại nhà</p>
            </div>
@@ -30,22 +30,22 @@ const newList = {
         <h1 class="font-semibold">HOẠT ĐỘNG SINH VIÊN</h1><br>
         <div class="flex justify-between">
           <div class="w-48 h-auto ">
-           <img class="w-full" src="./img/sp1.jfif" alt="">
+           <img class="w-full" src="http://channel.mediacdn.vn/prupload/879/2018/08/img20180813114743979.jpg" alt="">
            <h2 class="text-lg text-red-500">Sinh viên Fpoly được nghỉ dịch học online.</h2>
            <p class="text-sm mt-2">Sinh viên Fpoly được nghỉ dịch học online tại nhà inh viên Fpoly được nghỉ dịch học online tại nhàinh viên Fpoly được nghỉ dịch học online tại nhà</p>
           </div>
           <div class="w-48 h-auto ">
-           <img class="w-full" src="./img/sp1.jfif" alt="">
+           <img class="w-full" src="http://channel.mediacdn.vn/prupload/879/2018/08/img20180813114743979.jpg" alt="">
            <h2 class="text-lg text-red-500">Sinh viên Fpoly được nghỉ dịch học online.</h2>
            <p class="text-sm mt-2">Sinh viên Fpoly được nghỉ dịch học online tại nhà inh viên Fpoly được nghỉ dịch học online tại nhàinh viên Fpoly được nghỉ dịch học online tại nhà</p>
           </div>
           <div class="w-48 h-auto ">
-           <img class="w-full" src="./img/sp1.jfif" alt="">
+           <img class="w-full" src="http://channel.mediacdn.vn/prupload/879/2018/08/img20180813114743979.jpg" alt="">
            <h2 class="text-lg text-red-500">Sinh viên Fpoly được nghỉ dịch học online.</h2>
            <p class="text-sm mt-2">Sinh viên Fpoly được nghỉ dịch học online tại nhà inh viên Fpoly được nghỉ dịch học online tại nhàinh viên Fpoly được nghỉ dịch học online tại nhà</p>
           </div>
           <div class="w-48 h-auto ">
-           <img class="w-full" src="./img/sp1.jfif" alt="">
+           <img class="w-full" src="http://channel.mediacdn.vn/prupload/879/2018/08/img20180813114743979.jpg" alt="">
            <h2 class="text-lg text-red-500">Sinh viên Fpoly được nghỉ dịch học online.</h2>
            <p class="text-sm mt-2">Sinh viên Fpoly được nghỉ dịch học online tại nhà inh viên Fpoly được nghỉ dịch học online tại nhàinh viên Fpoly được nghỉ dịch học online tại nhà</p>
           </div>
